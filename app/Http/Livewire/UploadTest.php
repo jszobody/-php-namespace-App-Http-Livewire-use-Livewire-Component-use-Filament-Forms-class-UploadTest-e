7bf73d4a1f21599a1ae91870462bf7efafb3df58
Joseph Szobody <joseph@stechstudio.com>
2022-01-19 18:51:20 -0500

    commit

---
+++ app/Http/Livewire/UploadTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Filament\Forms;
+
+class UploadTest extends Component implements Forms\Contracts\HasForms
+{
+    use Forms\Concerns\InteractsWithForms;
+
+    public function render()
+    {
+        return view('livewire.upload-test');
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Forms\Components\FileUpload::make('attachment')
+        ];
+    }
+}
